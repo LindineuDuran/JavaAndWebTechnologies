94a63f0783bc15e970bbf2c28098be1d134ef847
Lindineu Lopes Duran <37485332+LindineuDuran@users.noreply.github.com>
2026-03-03 13:11:42 -0300

    API Todo List
    - Correção nas camadas de Serviço e Controler;
    - Packaging and Deploy;
    - Test of the Endpoints;

--- Java Enterprise Edition 8/hello-todo/src/main/java/br/com/lduran/rest/TodoRest.java
+++ Java Enterprise Edition 8/hello-todo/src/main/java/br/com/lduran/rest/TodoRest.java
@@ -4,11 +4,59 @@
  */
 package br.com.lduran.rest;
 
+import br.com.lduran.entity.Todo;
+import br.com.lduran.service.TodoService;
+import java.util.List;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+
 /**
  *
  * @author lsdur
  */
+@Path("todo")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class TodoRest
 {
+
+    @Inject
+    TodoService todoService;
+
+    @Path("new")
+    @POST
+    public Response createTodo(Todo todo)
+    {
+        todoService.createTodo(todo);
+        return Response.ok(todo).build();
+    }
+
+    @Path("update/{id}")
+    @PUT
+    public Response updateTodo(@PathParam("id") Long id, @RequestBody Todo todo)
+    {
+        todoService.updateTodo(id, todo);
+        return Response.ok(todo).build();
+    }
     
+    @Path("{id}")
+    @GET
+    public Todo getTodo(@PathParam("id") Long id){
+        return todoService.findToDoById(id);
+    }
+    
+    @Path("list")
+    @GET
+    public List<Todo> getTodos(){
+        return todoService.getTodos();
+    }
 }
--- Java Enterprise Edition 8/hello-todo/src/main/java/br/com/lduran/service/TodoService.java
+++ Java Enterprise Edition 8/hello-todo/src/main/java/br/com/lduran/service/TodoService.java
@@ -5,9 +5,12 @@
 package br.com.lduran.service;
 
 import br.com.lduran.entity.Todo;
+import java.util.List;
+import java.util.Optional;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
+import javax.ws.rs.NotFoundException;
 
 /**
  *
@@ -28,17 +31,35 @@ public class TodoService
         return todo;
     }
 
-    public Todo updateTodo(Todo todo)
+    public Todo updateTodo(Long id, Todo todo)
     {
-        // Persist into db
-        entityManager.merge(todo);
+        /*
+        ✔ Se a entidade já foi buscada → NÃO use merge
+        ✔ Apenas altere os atributos
+        ✔ O JPA faz o UPDATE automaticamente no commit da transação
+        */
+        Todo entity = findToDoById(id);
 
-        return todo;
+        entity.setTask(todo.getTask());
+        entity.setDueDate(todo.getDueDate());
+        entity.setCompleted(todo.isCompleted());
+        entity.setDateCreated(todo.getDateCreated());
+        entity.setDateComplited(todo.getDateComplited());
+
+        return entity;
     }
 
     public Todo findToDoById(Long id)
     {
-        return entityManager.find(Todo.class, id);
+        return Optional
+            .ofNullable(entityManager.find(Todo.class, id))
+            .orElseThrow(() ->
+                new NotFoundException("No records found for this ID!")
+            );
     }
 
+    public List<Todo> getTodos()
+    {
+        return entityManager.createQuery("SELECT t from Todo t", Todo.class).getResultList();
+    }
 }
