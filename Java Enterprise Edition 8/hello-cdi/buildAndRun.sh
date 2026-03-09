d73c2dd1da9a281eb84582ae72904c4487e8dcc7
Lindineu Lopes Duran <37485332+LindineuDuran@users.noreply.github.com>
2026-03-09 10:47:04 -0300

    hello-cdi
    - What is the CDI Container?;
    - Criação da classe RequestScope.java;

---
+++ Java Enterprise Edition 8/hello-cdi/buildAndRun.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+mvn clean package && docker build -t br.com.lduran/hello-cdi .
+docker rm -f hello-cdi || true && docker run -d -p 8080:8080 -p 4848:4848 --name hello-cdi br.com.lduran/hello-cdi
